31d9b2e02399b0920047cb94f0d5f33f9130d331
passwd10 <parkinseo94@naver.com>
2021-07-28 23:25:45 +0900

    feat: SEO 컴포넌트를 구현한다
    - meta 태그로 description, title, og 입력

---
+++ src/components/SEO.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+
+type Props = {
+  title: string;
+  description: string;
+  image?: string;
+}
+
+const SEO: React.FC<Props> = ({ title, description, image }) => {
+  return (
+    <Helmet>
+      <meta charSet='UTF-8' />
+      <meta name='description' content={description} />
+      <meta property='og:title' content={title}/>
+      <meta property='og:type' content='website'/>
+      <meta property='og:description' content={description}/>
+      <meta name='og:image' content={image} />
+      <title>{title}</title>
+    </Helmet>
+  );
+};
+
+export default SEO;
