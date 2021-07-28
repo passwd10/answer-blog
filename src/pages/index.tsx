8249cf713b8007cf5d0efc8bdd50d5db3a2bd8ac
passwd10 <parkinseo94@naver.com>
2021-07-29 01:02:29 +0900

    feat: 강아지 사진을 화면에 출력한다

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,31 @@
 import React from 'react';
 
+import { useStaticQuery, graphql } from 'gatsby';
+import Image from 'gatsby-image';
+
 import Layout from '../components/Layout';
 import SEO from '../components/SEO';
 
 const HomePage: React.FC = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      avatar: file(relativePath: {regex: "/dog.jpeg/"}) {
+        childImageSharp {
+          fixed {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `);
+
   return (
     <Layout>
       <SEO
         title={'HOME | Answer\'s blog'}
         description={'개발 흔적을 남기는 블로그'}
       />
-      <div>
-        Hello World!
-      </div>
+      <Image fixed={data.avatar.childImageSharp.fixed}/>
     </Layout>
   );
 };
