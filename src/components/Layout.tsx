df35beae79edf2634b435aa4c3fb1df9011d6a78
passwd10 <parkinseo94@naver.com>
2021-07-30 02:04:05 +0900

    feat: CSS를 초기화한다

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import styled from '@emotion/styled';
+import { css, Global } from '@emotion/core';
 
 type Props = {
   children: React.ReactNode;
@@ -9,10 +10,20 @@ type Props = {
 const Layout: React.FC<Props> = ({ children }) => {
   return (
     <StyledLayout>
+      <Global styles={reset}/>
       {children}
     </StyledLayout>
   );
 };
+
+const reset = css`
+  * {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+    font-family: Apple SD Gothic Neo;
+  }
+`;
 
 const StyledLayout = styled.div`
   width: 100%;
