2ee422bce5fede64be7345a3d8c1af3585e6cb7a
passwd10 <parkinseo94@naver.com>
2021-07-29 23:20:21 +0900

    feat: 강아지 이미지에 animation 효과준다

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,9 @@ import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 import Image from 'gatsby-image';
 
+import styled from '@emotion/styled';
+import { keyframes } from '@emotion/react';
+
 import Layout from '../components/Layout';
 import SEO from '../components/SEO';
 
@@ -40,9 +43,39 @@ const HomePage: React.FC = () => {
         description={'개발 흔적을 남기는 블로그'}
         image={data.avatar.childImageSharp.fixed.src}
       />
-      <Image fixed={data.avatar.childImageSharp.fixed}/>
+      <HomeContainer>
+        <ImageWrapper>
+          <Image
+            fixed={data.avatar.childImageSharp.fixed}
+            style={{ marginBottom: '-5px' }}
+          />
+        </ImageWrapper>
+      </HomeContainer>
     </Layout>
   );
 };
 
 export default HomePage;
+
+const HomeContainer = styled.section`
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const bounce = keyframes`
+  0% {
+    transform: translate(0, 0);
+  }
+
+  50% {
+    transform: translate(0, 10px);
+  }
+`;
+
+const ImageWrapper = styled.span`
+  animation: ${bounce} 3s ease infinite;
+  box-shadow: 0px 3px 5px 3px gray;
+`;
