3e87b419f99d832e69af0843adb54ee851710d7c
passwd10 <parkinseo94@naver.com>
2021-07-30 01:25:03 +0900

    feat: 홈페이지의 컴포넌트를 HomeContainer로 이관한다

--- src/components/HomeContainer.tsx
+++ src/components/HomeContainer.tsx
@@ -1,11 +1,47 @@
 import React from 'react';
 
-import styled from '@emotion/styled';
+import Image from 'gatsby-image';
 
-const HomeContainer: React.FC = () => {
+import styled from '@emotion/styled';
+import { keyframes } from '@emotion/react';
+
+import GitHubIcon from './icons/GitHubIcon';
+import MailIcon from './icons/MailIcon';
+
+type Props = {
+  data: {
+    avatar: {
+      childImageSharp: {
+        fixed: {
+          base64: string,
+          width: number,
+          height: number,
+          src: string,
+          srcSet: string,
+        }
+      }
+    }
+  }
+}
+
+const HomeContainer: React.FC<Props> = ({ data }) => {
   return (
     <Container>
-      Home
+      <ImageWrapper>
+        <Image
+          fixed={data.avatar.childImageSharp.fixed}
+          style={{ marginBottom: '-5px' }}
+        />
+      </ImageWrapper>
+      <Spacer />
+      <Socials>
+        <li>
+          <GitHubIcon/>
+        </li>
+        <li>
+          <MailIcon />
+        </li>
+      </Socials>
     </Container>
   );
 };
@@ -17,6 +53,30 @@ const Container = styled.section`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+`;
+
+const bounce = keyframes`
+  0% {
+    transform: translate(0, 0);
+  }
+
+  50% {
+    transform: translate(0, 10px);
+  }
+`;
+
+const ImageWrapper = styled.span`
+  animation: ${bounce} 3s ease infinite;
+  box-shadow: 0px 3px 5px 3px gray;
+`;
+
+const Socials = styled.ul`
+  display: flex;
+  list-style: none;
+`;
+
+const Spacer = styled.div`
+  margin: 1em;
 `;
 
 export default HomeContainer;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 
 import { useStaticQuery, graphql } from 'gatsby';
-import Image from 'gatsby-image';
-
-import styled from '@emotion/styled';
-import { keyframes } from '@emotion/react';
 
 import Layout from '../components/Layout';
 import SEO from '../components/SEO';
+import HomeContainer from '../components/HomeContainer';
 
 type QueryProps = {
   avatar: {
@@ -43,39 +40,9 @@ const HomePage: React.FC = () => {
         description={'개발 흔적을 남기는 블로그'}
         image={data.avatar.childImageSharp.fixed.src}
       />
-      <HomeContainer>
-        <ImageWrapper>
-          <Image
-            fixed={data.avatar.childImageSharp.fixed}
-            style={{ marginBottom: '-5px' }}
-          />
-        </ImageWrapper>
-      </HomeContainer>
+      <HomeContainer data={data}/>
     </Layout>
   );
 };
 
 export default HomePage;
-
-const HomeContainer = styled.section`
-  width: 100%;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const bounce = keyframes`
-  0% {
-    transform: translate(0, 0);
-  }
-
-  50% {
-    transform: translate(0, 10px);
-  }
-`;
-
-const ImageWrapper = styled.span`
-  animation: ${bounce} 3s ease infinite;
-  box-shadow: 0px 3px 5px 3px gray;
-`;
