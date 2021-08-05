3b61daad90d8b45f2c3e4ed9524d97dec9457d45
passwd10 <parkinseo94@naver.com>
2021-08-06 00:07:08 +0900

    feat: 공통 font-family를 지정한다

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,6 +3,11 @@ import React from 'react';
 import styled from '@emotion/styled';
 import { css, Global } from '@emotion/core';
 
+import Merienda from '../fonts/Merienda.ttf';
+import NotoSansKR from '../fonts/NotoSansKR.otf';
+import Hahmlet from '../fonts/Hahmlet.ttf';
+import DancingScript from '../fonts/DancingScript.ttf';
+
 import GNB from './GNB';
 
 type Props = {
@@ -20,13 +25,36 @@ const Layout: React.FC<Props> = ({ children }) => {
 };
 
 const reset = css`
+  @font-face {
+    font-family: 'Merienda';
+    src: url('${Merienda}') format('truetype');
+  }
+
+  @font-face {
+    font-family: 'NotoSansKR';
+    src: url('${NotoSansKR}') format('opentype');
+  }
+
+  @font-face {
+    font-family: 'Hahmlet';
+    src: url('${Hahmlet}') format('truetype');
+  }
+
+  @font-face {
+    font-family: 'DancingScript';
+    src: url('${DancingScript}') format('truetype')
+  }
+
   * {
     box-sizing: border-box;
     padding: 0;
     margin: 0;
-    font-family: Apple SD Gothic Neo;
     outline: none;
     color: #000000;
+  }
+
+  body {
+    font-family: 'NotoSansKR';
   }
 
   a {
@@ -41,7 +69,6 @@ const reset = css`
 const StyledLayout = styled.div`
   width: 100%;
   min-height: 100vh;
-  background: #FFF;
 `;
 
 export default Layout;
